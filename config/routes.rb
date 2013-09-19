f5bb30b721861c987a77ab1d93c8a51878282619
Apprentice <apprentice@dbc-chicago-001.local>
2013-09-19 10:23:30 -0500

    added controller, views, routes for feed and comments. Moved the current_user method to the application_controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ SocialTrainer::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root :to => 'users#index'
 
-  resources :users
+  resources :sessions, only: [:create, :new, :destroy]
+  resources :users do
+    resources :posts do
+      resources :comments
+    end
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
