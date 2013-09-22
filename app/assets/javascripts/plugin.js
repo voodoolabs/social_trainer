fb30e1128a5253a9a9166fc508bfbeed0acd5a5d
Apprentice <apprentice@dbc-chicago-001.local>
2013-09-22 10:35:26 -0500

    .

--- app/assets/javascripts/plugin.js
+++ app/assets/javascripts/plugin.js
@@ -4,5 +4,21 @@ $(document).ready(function(){
     $('form.loginform').hide();
     $('form.signupform').show();
     $(this).hide();
-  })
-})
+  });
+
+  $('.vote').on("click", function(event){
+    event.preventDefault();
+    var url = $(this).attr('href');
+    var post = $(this).data('post-id');
+    var vote = $(this).data('vote');
+    var data = {post_id: post, value: vote};
+    // debugger
+    
+    $.post(url, data, function(response){
+      debugger
+      console.log(response)
+      console.log(response['score'])
+      $('div.score').html("response['score']");
+    });
+  });
+});
